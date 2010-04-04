da035e4c5c05bde385e2a30e2b8737c243eca50a
Michael Melanson <michael@codeshack.ca>
2010-04-04 19:42:25 -0400

    Fixed last commit

--- src/site.erl
+++ src/site.erl
@@ -16,7 +16,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {site, interval, events, etag, timer}).
+-record(state, {site, interval, events, etag}).
 
 -include("envcan_api.hrl").
 
